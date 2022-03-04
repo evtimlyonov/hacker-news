dc84e20acd4b1c682782de7f47b1ce02c8233f37
Evtim Lyonov <evtim.lyonov@gmail.com>
2022-03-04 23:47:12 +0200

    Testing the loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='relative w-28 h-28 mx-auto mt-40 rounded-full border-4 border-[#ccc] border-t-[#49a6e9] animate-spin'></div>
+      <div className='block mx-auto text-center -mt-[68px] text-gray-600 animate-pulse'>
+        Loading...
+      </div>
     </div>
   );
 }
